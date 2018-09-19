15b909316e34d5222bef552fa0f9b1ff147fea8d
vihaxia <871414663@qq.com>
2018-09-19 14:42:50 +0800

    composer包基础文件

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//
+require_once './vendor/autoload.php';
+//
+use Tools\TestTools;
+
+echo TestTools::say();
+
+
---
+++ src/Tools/TestTools.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tools;
+
+class testTools {
+
+    public function __construct()
+    {
+        return 'success';
+    }
+
+    public static function say()
+    {
+        return 'hello world';
+    }
+    
+}
